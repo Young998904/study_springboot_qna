c8de3e12df2e02f096951be3efee2b288ca2e20c
Young998904 <like0998904@gmail.com>
2022-10-06 15:38:40 +0900

    :sparkles: GetMapping & PostMapping 비교

--- src/main/java/com/ay/study/qna/MainController.java
+++ src/main/java/com/ay/study/qna/MainController.java
@@ -1,7 +1,10 @@
 package com.ay.study.qna;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -16,5 +19,26 @@ public class MainController {
     @ResponseBody
     public String ShowSbb() {
         return "Hello qna";
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String showGetTest() {
+        return """
+            <h1> GetMapping 으로 들어옴 </h1>
+            <form method="POST" action="/test">
+                <input type="number" name="age" placeholder="나이를 입력하세요" />
+                <input type="submit" value="test 페이지에 POST 방식으로 이동" />
+            </form>
+            """;
+    }
+
+    @PostMapping("/test")
+    @ResponseBody
+    public String showPostTest(@RequestParam(defaultValue = "0") int age) {
+        return """
+            <h1> PostMapping 으로 들어옴 </h1>
+            <h3> 입력된 나이 : %d </h3>
+            """.formatted(age);
     }
 }
