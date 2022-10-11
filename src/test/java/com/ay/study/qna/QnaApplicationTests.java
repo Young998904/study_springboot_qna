a0e682eb748e321747b94a903363046076d135ea
Young998904 <like0998904@gmail.com>
2022-10-11 13:46:18 +0900

    :white_check_mark: getAnswerList (@Transactional)

--- src/test/java/com/ay/study/qna/QnaApplicationTests.java
+++ src/test/java/com/ay/study/qna/QnaApplicationTests.java
@@ -4,10 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -48,5 +50,16 @@ class SbbApplicationTests {
         a1.setCreateDate(LocalDateTime.now());
         a1.setQuestion(q);
         answerRepository.save(a1);
+    }
+
+    @Transactional // 테스트 시에만 DB 세션이 findById 수행 후 종료되어서 생기는 문제 해결 (세션 유지 어노테이션)
+    @Test
+    void testJpa4() {
+        Optional<Question> oq = questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswerList();
+        assertEquals(1, answerList.size());
     }
 }
