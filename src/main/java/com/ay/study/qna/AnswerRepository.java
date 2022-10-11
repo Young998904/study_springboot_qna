1c5217576689be3f913357e51cdcf1350fa4d427
Young998904 <like0998904@gmail.com>
2022-10-11 11:10:37 +0900

    :package: AnswerRepository, QuestionRepository 생성

---
+++ src/main/java/com/ay/study/qna/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.ay.study.qna;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
---
+++ src/main/java/com/ay/study/qna/QuestionRepository.java
@@ -0,0 +1,7 @@
+package com.ay.study.qna;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+}
