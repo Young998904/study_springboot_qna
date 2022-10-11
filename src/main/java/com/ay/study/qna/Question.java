cccbcda17befde589e1c7220e54c24d0ac0f4fb1
Young998904 <like0998904@gmail.com>
2022-10-11 10:49:16 +0900

    :card_file_box: Question Entity 에 Answer 의존성 추가 (필수 내용 x)

--- src/main/java/com/ay/study/qna/Question.java
+++ src/main/java/com/ay/study/qna/Question.java
@@ -1,5 +1,6 @@
 package com.ay.study.qna;
 
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,9 +15,15 @@ public class Question {
     @Id // primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
     private Integer id;
+
     @Column(length = 200) // varchar(200)
     private String subject;
+
     @Column(columnDefinition = "TEXT")
     private String content;
+
     private LocalDateTime createDate;
+
+    @OneToMany (mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
