f1ff61f6248eaa35f3b094b20f83816ad2ac9ca0
Young998904 <like0998904@gmail.com>
2022-10-12 11:16:04 +0900

    :zap: 외래키 비활성화 중복 해결 (1) RepositoryUtil 사용

--- src/main/java/com/ay/study/qna/AnswerRepository.java
+++ src/main/java/com/ay/study/qna/AnswerRepository.java
@@ -1,11 +1,12 @@
 package com.ay.study.qna;
 
+import com.ay.study.qna.base.RepositoryUtil;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+public interface AnswerRepository extends JpaRepository<Answer, Integer>, RepositoryUtil {
     @Transactional
     @Modifying // @Query 에 의해 작성된 데이터 변경이 일어나는 삽입, 수정, 삭제 쿼리 메소드를 사용할 때 필요
     @Query(value = "truncate answer", nativeQuery = true)
--- src/main/java/com/ay/study/qna/QuestionRepository.java
+++ src/main/java/com/ay/study/qna/QuestionRepository.java
@@ -1,28 +1,19 @@
 package com.ay.study.qna;
 
+import com.ay.study.qna.base.RepositoryUtil;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface QuestionRepository extends JpaRepository<Question, Integer> {
+public interface QuestionRepository extends JpaRepository<Question, Integer>, RepositoryUtil {
     Question findBySubject(String subject);
 
     @Transactional
     @Modifying // @Query 에 의해 작성된 데이터 변경이 일어나는 삽입, 수정, 삭제 쿼리 메소드를 사용할 때 필요
     @Query(value = "truncate question", nativeQuery = true)
     void truncate();
-
-    @Transactional
-    @Modifying
-    @Query(value = "SET FOREIGN_KEY_CHECKS = 0", nativeQuery = true)
-    void disableForeignKeyChecks();
-
-    @Transactional
-    @Modifying
-    @Query(value = "SET FOREIGN_KEY_CHECKS = 1", nativeQuery = true)
-    void enableForeignKeyChecks();
 
     Question findBySubjectAndContent(String s1, String s2);
 
